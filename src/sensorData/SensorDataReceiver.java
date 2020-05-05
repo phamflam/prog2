d7c65ccc0c24d245c6413d66b0fcd7f041877b87
phamflam <lynnphamthu@gmail.com>
2020-05-05 21:38:40 +0200

    commit

---
+++ src/sensorData/SensorDataReceiver.java
@@ -0,0 +1,38 @@
+package sensorData;
+
+import streamMachine.PersistenceException;
+import streamMachine.StreamMachine;
+import transmission.DataConnection;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class SensorDataReceiver {
+    private final DataConnection connection;
+    private final StreamMachine storage;
+
+    public SensorDataReceiver(DataConnection connection, StreamMachine storage) {
+        this.connection = connection;
+        this.storage = storage;
+    }
+
+    private void readDataSet() throws IOException, PersistenceException {
+        DataInputStream dis=this.connection.getDataInputStream();
+        //read from tcp
+        String name= dis.readUTF();
+        long time = dis.readLong();
+        int len=dis.readInt();
+        float[] values= new float[len];
+        for(int i=0; i<len;i++){
+            dis.readFloat();
+        }
+
+        //write into machine
+        this.storage.saveData(time, values);
+    }
+
+    StreamMachine getStorage() {
+        return storage;
+    }
+}
---
+++ src/sensorData/SensorDataSender.java
@@ -0,0 +1,31 @@
+package sensorData;
+
+import transmission.DataConnection;
+import transmission.DataConnector;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class SensorDataSender {
+    private final DataConnection connection;
+
+    public SensorDataSender(DataConnection connection) {
+        this.connection = connection;
+    }
+
+    public void sendData(String name, long time, float[] values) throws IOException {
+        if(name==null  || time<0 || values==null| values.length==0)
+            throw new IOException("parameter must not be null or empty");
+
+        DataOutputStream dos = this.connection.getDataOutputStream();
+
+        dos.writeUTF(name);
+        dos.writeLong(time);
+        dos.writeFloat(values.length);
+        for (int i=0;i<values.length;i++) {
+            dos.writeFloat(values[i]);
+        }
+
+    }
+}
